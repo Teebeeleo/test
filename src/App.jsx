309afc591887844d88d3d19878ff14ade8d9794f
Teebeeleo <adesoyetoyeeb01@gmail.com>
2026-04-10 08:22:47 -0400

    undo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import Work from "./Work";
 import Home from "./Home";
 import "./App.css";
